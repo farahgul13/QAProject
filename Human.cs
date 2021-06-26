47bb300c442d9876d4f0ff1140754f1e6ec77c8d
Farah Gul <farah.gul@mopssys.com>
2021-06-27 03:15:23 +0500

    3

---
+++ Human.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QAProject
+{
+   public class Human
+    {
+        public static int X = 0;
+        public static void Walk(string action)
+        {
+            Console.WriteLine(action + X );
+        }
+    }
+}
